3a883fb9cce272e0160f52af36ac7af36cb84a1c
Ugo-M <ugo.mekki@gmail.com>
2019-09-06 18:59:09 +0200

    nettoyage du code et ajout des requetes update et delete

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./queries');
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+});
 
-app.get('/departements', db.getDepartements)
-app.get('/departements/:name', db.getDepartementByName)
-app.post('/departements', db.createDepartement)
-app.get('/personnel', db.getPersonnel)
-app.get('/personnel/:name', db.getPersonByName)
-app.post('/personnel', db.createPerson)
+app.get('/departments', db.getDepartments);
+app.get('/departments/:name', db.getDepartmentByName);
+app.get('/personnel', db.getPersonnel);
+app.get('/personnel/:name', db.getPersonByName);
+
+app.post('/departments', db.createDepartment);
+app.post('/personnel', db.createPerson);
+
+app.put('/departments/:id', db.updateDepartment);
+app.put('/personnel/:id', db.updatePerson);
+
+app.delete('/departments/:id', db.deleteDepartment);
+app.delete('/personnel/:id', db.deletePerson);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+    console.log(`App running on port ${port}.`)
+});
 
 
-// requetes curl :
+// curl requests :
 
-//curl --data "name=doe&firstname=john&birthdate=2000-01-01&departement=Informatique" http://localhost:3000/personnel/
+// INSERT
+// curl --data "name=doe&firstname=john&birthdate=2000-01-01&department=Informatique" http://localhost:3000/personnel/
+// curl --data "name=test" http://localhost:3000/departments/
 
-//curl --data "name=test" http://localhost:3000/departements/
+// UPDATE
+// le /6 correspond à l'id
+// curl -X PUT -d "name=newname" http://localhost:3000/departments/6
+// curl -X PUT -d "name=newname" -d "firstname=newfirstname" http://localhost:3000/personnel/6
+
+// DELETE
+// le /6 correspond à l'id
+// curl -X "DELETE" http://localhost:3000/departments/6
+// curl -X "DELETE" http://localhost:3000/personnel/6
--- queries.js
+++ queries.js
@@ -1,79 +1,129 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'ugo-m_m5301',
-  host: 'postgresql-ugo-m.alwaysdata.net',
-  database: 'ugo-m_m5301',
-  password: 'Abc.123',
-  port: 5432,
-})
+    user: 'ugo-m_m5301',
+    host: 'postgresql-ugo-m.alwaysdata.net',
+    database: 'ugo-m_m5301',
+    password: 'Abc.123',
+    port: 5432,
+});
 
-const getDepartements = (request, response) => {
-  pool.query('SELECT * FROM departement', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+const getDepartments = (request, response) => {
+    pool.query('SELECT * FROM department', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+};
 
-const getDepartementByName = (request, response) => {
-  const name = request.params.name
+const getDepartmentByName = (request, response) => {
+    const name = request.params.name;
 
-  pool.query('SELECT * FROM departement WHERE name = $1', [name], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+    pool.query('SELECT * FROM department WHERE name = $1', [name], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+};
 
 const getPersonnel = (request, response) => {
-  pool.query('SELECT * FROM personnel', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+    pool.query('SELECT * FROM personnel', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+};
 
 const getPersonByName = (request, response) => {
-  const name = request.params.name
+    const name = request.params.name;
 
-  pool.query('SELECT * FROM personnel WHERE name = $1', [name], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+    pool.query('SELECT * FROM personnel WHERE name = $1', [name], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+};
 
-const createDepartement = (request, response) => {
-  const { name} = request.body
+const createDepartment = (request, response) => {
+    const {name} = request.body;
 
-  pool.query('INSERT INTO departement (name) VALUES ($1)', [name], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`Departement added with name: ${name}`)
-  })
-}
+    pool.query('INSERT INTO department (name) VALUES ($1)', [name], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Department added with name: ${name}`)
+    })
+};
 
 const createPerson = (request, response) => {
-  const { name, firstname, birthdate, departement } = request.body
+    const { name, firstname, birthdate, department } = request.body;
 
-  pool.query('INSERT INTO personnel (name, firstname, birthdate, departement) VALUES ($1, $2, $3, $4)', [name, firstname, birthdate, departement], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`Personne added with name: ${name}`)
-  })
-}
+    pool.query('INSERT INTO personnel (name, firstname, birthdate, department) VALUES ($1, $2, $3, $4)', [name, firstname, birthdate, department], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Person added with name: ${name}`)
+    })
+};
+
+const updateDepartment = (request, response) => {
+    const id = parseInt(request.params.id);
+    const {name} = request.body;
+
+    pool.query('UPDATE department SET name = $1 WHERE id = $2', [name, id], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+    })
+};
+
+const updatePerson = (request, response) => {
+    const id = parseInt(request.params.id);
+    const {name, firstname} = request.body;
+
+    pool.query('UPDATE personnel SET name = $1, firstname = $2 WHERE id = $3', [name, firstname, id], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+    })
+};
+
+const deleteDepartment = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('DELETE from department WHERE id = $1', [id], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+    })
+};
+
+const deletePerson = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('DELETE from personnel WHERE id = $1', [id], (error) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+    })
+};
 
 module.exports = {
-getDepartements,
-getPersonnel,
-getPersonByName,
-getDepartementByName,
-createPerson,
-createDepartement,
-}
+    getDepartments,
+    getPersonnel,
+    getPersonByName,
+    getDepartmentByName,
+    createPerson,
+    createDepartment,
+    updateDepartment,
+    updatePerson,
+    deleteDepartment,
+    deletePerson,
+};
