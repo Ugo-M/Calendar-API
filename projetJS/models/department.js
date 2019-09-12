3808a5cf4511d3220a20c06a23784af4c2df1e31
Ugo-M <ugo.mekki@gmail.com>
2019-09-12 18:59:37 +0200

    nettoyage du code

--- projetJS/models/department.js
+++ projetJS/models/department.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Department = sequelize.define('Department', {
-    department_name: DataTypes.STRING
-  }, {});
-  Department.associate = function(models) {
-    Department.hasMany(models.Person, {
-      foreignKey: 'department_id',
-      as: 'persons',
+    const Department = sequelize.define('Department', {
+        department_name: DataTypes.STRING
     });
-  };
-  return Department;
+    Department.associate = function (models) {
+        Department.hasMany(models.Person, {
+            foreignKey: 'department_id',
+            as: 'persons',
+        });
+    };
+    return Department;
 };
