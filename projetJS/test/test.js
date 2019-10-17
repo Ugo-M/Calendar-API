b349e0f7c3c84d28c365ce4bbbcdb7c6035781f4
Ugo-M <ugo.mekki@etu.univ-amu.fr>
2019-10-17 15:07:00 +0200

    Cleanup
    Signed-off-by: Ugo <ugo.mekki@gmail.com>

--- projetJS/test/test.js
+++ projetJS/test/test.js
@@ -38,10 +38,10 @@ describe("API test",function(){
             .expect("Content-type", /json/)
             .expect(201)
             .end(function (err, res) {
-                res.tus.should.equal(201);
+                res.status.should.equal(201);
                 done();
             });
-    });sta
+    });
 
     it("should login as the newly created user",function(done) {
         server
